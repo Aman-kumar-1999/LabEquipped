1dbff93671f23523cf028be1f2d6331c9a442fa7
Aman Kumar <78886335+Aman-kumar-1999@users.noreply.github.com>
2025-09-07 12:55:30 +0530

    added Backend code

---
+++ Backend/labequiped/src/main/java/com/labequiped/labequiped/LabequipedApplication.java
@@ -0,0 +1,13 @@
+package com.labequiped.labequiped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabequipedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabequipedApplication.class, args);
+	}
+
+}
---
+++ Backend/labequiped/src/test/java/com/labequiped/labequiped/LabequipedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labequiped.labequiped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabequipedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
