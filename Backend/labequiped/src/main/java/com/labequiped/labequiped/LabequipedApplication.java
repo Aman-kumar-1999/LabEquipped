914f0f2f10aa941263b6e68aca42f5e20bf2d9f4
Aman Kumar <78886335+Aman-kumar-1999@users.noreply.github.com>
2025-09-07 13:03:27 +0530

    added Fronted

--- Backend/labequiped/src/main/java/com/labequiped/labequiped/LabequipedApplication.java
+++ Backend/labequiped/src/main/java/com/labequiped/labequiped/LabequipedApplication.java
@@ -8,6 +8,7 @@ public class LabequipedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabequipedApplication.class, args);
+		System.out.println("Aplicação iniciada com sucesso!");
 	}
 
 }
