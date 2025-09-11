3a423ba041393113b9025ab48737d2d79e6d624c
Aman Kumar <78886335+Aman-kumar-1999@users.noreply.github.com>
2025-09-11 11:13:00 +0530

    main connection done

--- Backend/ServiceRegistory/src/main/java/com/labequiped/serviceregistry/ServiceRegistoryApplication.java
+++ Backend/ServiceRegistory/src/main/java/com/labequiped/serviceregistry/ServiceRegistoryApplication.java
@@ -1,5 +1,9 @@
 package com.labequiped.serviceregistry;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,7 +14,20 @@ public class ServiceRegistoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistoryApplication.class, args);
+		
+		
+		
+		
 		System.out.println("Application is Running ...");
+		
+		
+		List<Employee> lisEmployee = new ArrayList<>();
+		
+		
+		
+		
+		
+		
 	}
 
 }
